4b9cef0fd18945f8b9cb249f20f35a340a53e020
Daehan Kim <really@iamghost.kr>
2018-03-20 18:52:09 +0900

    Update wtf-bluehouse.user.js

--- wtf-bluehouse.user.js
+++ wtf-bluehouse.user.js
@@ -8,17 +8,14 @@
 
 var REQUIRED_VOTES = 200000;
 
+var votesText = document.querySelector(".cspv_participant span").innerHTML;
+var votes = Number(votesText.replace(/,/g, ''));
 
-window.addEventListener('load', function() {
-  var votesText = document.querySelector(".cspv_participant span").innerHTML;
-  var votes = Number(votesText.replace(/,/g, ''));
-  
-  var daysLeftElement = document.querySelector(".cspvp_day");
-  
-  var daysLeftText = daysLeftElement.innerHTML;
-  
-  document.querySelector(".cspv_participant").innerHTML = '청원 참여(' + daysLeftText + ')';
-	daysLeftElement.innerHTML = votesText + '/' + Number(REQUIRED_VOTES).toLocaleString() + '명 참여';
-  
-  document.querySelector(".cspvp_ing").style.width = String(votes / REQUIRED_VOTES * 100) + "%";
-}, false);
+var daysLeftElement = document.querySelector(".cspvp_day");
+
+var daysLeftText = daysLeftElement.innerHTML;
+
+document.querySelector(".cspv_participant").innerHTML = '청원 참여(' + daysLeftText + ')';
+daysLeftElement.innerHTML = votesText + '/' + Number(REQUIRED_VOTES).toLocaleString() + '명 참여';
+
+document.querySelector(".cspvp_ing").style.width = String(votes / REQUIRED_VOTES * 100) + "%";
